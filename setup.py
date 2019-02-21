295068c8cdf23a4b39b47f1711fb8c0f5d7deb6d
SimoneBronzini <simone.bronzini@gmail.com>
2019-02-21 22:22:03 +0100

    add upstream url and short description

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='chainside-webpos-sdk',
-      version='1.0.0',
+      version='1.0.1',
       packages=find_packages(),
       install_requires=['moveax-sdk-boilerplate'],
+      url='https://github.com/chainside/webpos-sdk-python',
+      description='Python SDK to integrate ChainsidePay webPOS.'
       )
