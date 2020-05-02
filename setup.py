f169aaec16bb5a267cb4eddca4d81b96ea2643dc
Francesco <francesco.andreozzi90@gmail.com>
2020-05-02 19:30:09 +0200

    pin moveax sdk boilerplate to 1.1.1 version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='chainside-webpos-sdk',
-      version='2.0.0',
+      version='2.0.1',
       packages=find_packages(),
-      install_requires=['moveax-sdk-boilerplate==1.1.0'],
+      install_requires=['moveax-sdk-boilerplate==1.1.1'],
       url='https://github.com/chainside/webpos-sdk-python',
       description='Python SDK to integrate ChainsidePay webPOS.'
       )
