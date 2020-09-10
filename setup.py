31e31562b0a68b740dfe833e72b47b4bed4b31e6
dozeee <francesco.andreozzi90@gmail.com>
2020-09-10 11:20:32 +0200

    bump sdk boilerplate version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='chainside-webpos-sdk',
-      version='2.0.1',
+      version='2.1.0',
       packages=find_packages(),
-      install_requires=['moveax-sdk-boilerplate==1.1.1'],
+      install_requires=['moveax-sdk-boilerplate==1.2.0'],
       url='https://github.com/chainside/webpos-sdk-python',
       description='Python SDK to integrate ChainsidePay webPOS.'
       )
